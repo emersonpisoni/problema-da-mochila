97b32799ac85e9bd5ba0b7e302160d1fab60f69a
Emerson Pisoni <emersonpisoni14@gmail.com>
2020-07-02 18:42:06 -0300

    Updates

---
+++ precache-manifest.120c686e4e47dcedbe0ffdde8dce61f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bec96eba6af81745915eaf222334f7",
+    "url": "/problema-da-mochila/index.html"
+  },
+  {
+    "revision": "be23e8ba36e1aaeecfe2",
+    "url": "/problema-da-mochila/static/css/main.941b720e.chunk.css"
+  },
+  {
+    "revision": "1fde9582dac51ef6b521",
+    "url": "/problema-da-mochila/static/js/2.e67a320e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/problema-da-mochila/static/js/2.e67a320e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be23e8ba36e1aaeecfe2",
+    "url": "/problema-da-mochila/static/js/main.481d5589.chunk.js"
+  },
+  {
+    "revision": "a955fa45289c9de39445",
+    "url": "/problema-da-mochila/static/js/runtime-main.ad1ce5b8.js"
+  }
+]);
